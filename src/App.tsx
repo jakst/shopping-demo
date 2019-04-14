1d18c645a5a41c8a90070a7cbb3c8f7c289115dc
Jakob Ståhl <jakob.stahl91@gmail.com>
2019-04-14 12:26:57 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Route, Switch } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
 	return (
 		<div>
-			<h1>Shopping</h1>
+			<a href="/">
+				<h1>Shopping.com</h1>
+			</a>
+
+			<BrowserRouter>
+				<Suspense fallback={<LoadingMessage />}>
+					<Switch>
+						<Route exact path="/">
+							<div>
+								<h2>Main page</h2>
+							</div>
+						</Route>
+
+						<Route path="/product/:id">
+							<div>
+								<h2>Product page</h2>
+							</div>
+						</Route>
+
+						<Route path="/checkout">
+							<div>
+								<h2>Checkout page</h2>
+							</div>
+						</Route>
+
+						<Route path="/">
+							<div>
+								<h2>404 not found</h2>
+							</div>
+						</Route>
+					</Switch>
+				</Suspense>
+			</BrowserRouter>
 		</div>
 	)
+}
+
+function LoadingMessage() {
+	return <div>Laddar</div>
 }
