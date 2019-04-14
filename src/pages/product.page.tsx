ba16ccd810e189466805446a60d5f2d469c71de0
Jakob Ståhl <jakob.stahl91@gmail.com>
2019-04-14 11:26:16 +0200

    Read product id from url

--- src/pages/product.page.tsx
+++ src/pages/product.page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { match } from 'react-router'
 
-export default function ProductPage() {
+type Props = {
+	match: match<{ id: string }>
+}
+
+export default function ProductPage({ match }: Props) {
+	const productId = match.params.id
+
 	return (
 		<div>
 			<h2>Product page</h2>
+			Product id: {productId}
 		</div>
 	)
 }
