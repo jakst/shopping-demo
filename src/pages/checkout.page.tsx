c16d2bd8f97c01d72a18a2483d262a767ecdbcb3
Jakob Ståhl <jakob.stahl91@gmail.com>
2019-04-16 21:41:15 +0200

    List cart products in checkout

--- src/pages/checkout.page.tsx
+++ src/pages/checkout.page.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { useCart } from '../cart/cart.context'
 
 export default function CheckoutPage() {
+	const { cartItems } = useCart()
+
+	if (!cartItems.length)
+		return (
+			<Wrapper>
+				<h2>Your cart is empty</h2>
+
+				<Link to="/">Go to products overview 👉</Link>
+			</Wrapper>
+		)
+
 	return (
-		<div>
-			<h2>Checkout page</h2>
-		</div>
+		<Wrapper>
+			{cartItems.map(item => (
+				<Item key={item.product.id}>
+					<div>{item.product.name}</div>
+					<div>
+						{item.quantity} x {item.product.price} = {item.value} kr
+					</div>
+				</Item>
+			))}
+		</Wrapper>
 	)
 }
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	max-width: 500px;
+	margin: 0 auto;
+	padding: 16px;
+	box-shadow: 1px 1px 3px #ddd;
+`
+
+const Item = styled.div`
+	display: flex;
+	justify-content: space-between;
+`
